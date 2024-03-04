52a8b56e483a7ef4237e72a18e421439a8972998
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2024-03-04 12:44:48 -0600

    Base Exceptions
    Added custom class exceptions

---
+++ App/Core/Exceptions/AppException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Core\Exceptions;
+
+use Exception;
+use Throwable;
+use App\Core\Server\Logger;
+use App\Core\Application\Configuration;
+
+class AppException extends Exception
+{
+
+    public function __construct($message, $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        if (Configuration::AUTOLOG_EXCEPTIONS) {
+            Logger::LogError("(Autologger)" . Logger::class, $this->__toString() . $this->getTraceAsString());
+        }
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
