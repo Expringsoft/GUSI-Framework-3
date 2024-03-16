4868632118c7472f75cafff6923d908a00334fc3
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2024-03-15 20:02:31 -0600

    Home class and SampleAPI implementation to follow new changes for Channels and Modules

--- App/Apis/v1/Sample/SampleAPI.php
+++ App/Apis/v1/Sample/SampleAPI.php
@@ -5,12 +5,14 @@ use App\Core\Application\SharedConsts;
 use App\Core\Framework\Abstracts\Api;
 use App\Core\Framework\Enumerables\RequestMethods;
 use App\Core\Framework\Structures\APIResponse;
-use Random\RandomException;
+use App\Core\Framework\Enumerables\Channels;
+use App\Modules\Index\APIs_Module;
 
 class SampleAPI extends Api{
 
 	public function Main(...$args)
 	{
+		self::setChannel(Channels::BETA);
 		if (!$this->isRequestMethodAllowed(RequestMethods::GET)) {
 			http_response_code(SharedConsts::HTTP_RESPONSE_METHOD_NOT_ALLOWED);
 			$this->buildResponse(new APIResponse(SharedConsts::HTTP_RESPONSE_METHOD_NOT_ALLOWED, "Method not allowed"));
@@ -21,11 +23,16 @@ class SampleAPI extends Api{
 			$this->buildResponse(new APIResponse(SharedConsts::HTTP_RESPONSE_BAD_REQUEST, "Invalid version"));
 			return;
 		}
-		$this->buildResponse(new APIResponse(SharedConsts::HTTP_RESPONSE_OK, "Hello from Sample Api"));
+		$this->buildResponse(new APIResponse(SharedConsts::HTTP_RESPONSE_OK, "Hello from Sample Api", ["API Channel" => self::getChannel()->name, "Module Channel" => self::getModuleChannel()->name]));
 	}
 
 	public static function getParentModule()
 	{
-		return \App\Modules\Index\APIs_Module::class;
+		return APIs_Module::class;
+	}
+
+	public static function getModuleChannel(): Channels
+	{
+		return self::getParentModule()::getChannel();
 	}
 }
--- App/Controllers/Index/Home.php
+++ App/Controllers/Index/Home.php
@@ -2,17 +2,27 @@
 namespace App\Controllers\Index;
 
 use App\Core\Framework\Abstracts\Controller;
+use App\Core\Framework\Enumerables\Channels;
 use App\Modules\Index\Index_Module;
 
 class Home extends Controller
 {
 	public function Main(...$args)
 	{
+		$this::setChannel(Channels::PROD);
+
+		echo self::getChannel()->name;
+		echo self::getModuleChannel()->name;
 		$this->setView('Default/Home.php');
 	}
 
-	public static function getParentModule()
+	public static function getParentModule(): string
 	{
 		return Index_Module::class;
+	}
+
+	public static function getModuleChannel(): Channels
+	{
+		return self::getParentModule()::getChannel();
 	}
 }
