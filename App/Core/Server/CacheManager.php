fcfbd4f0f45715438a32eac7c2ef04200fde9359
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2024-07-08 18:11:50 -0600

    feat(CacheManager): CacheManager implementation
    - Stores serialized data with the format [data] (which contains the data) and [expires] where the expiration time in seconds is stored using the function time() + the cache duration seconds.
    - To get or write the cache, a key is specified whose value is converted to MD5, forming the cache file name.
    - Cached data is not stored if the FileManager condition isStorageUsageWithinLimits is not met.
    - The maximum cache size (per file) can be set in the Configuration class. The cache folder location can also be configured; by default, it is located in App/Cache. The cache extension can also be configured; by default, it is .cache.
    - Functions to clear all cache and expired cache, with potential to use them in routines or scheduled tasks.

---
+++ App/Core/Server/CacheManager.php
@@ -0,0 +1,123 @@
+<?php
+namespace App\Core\Server;
+
+use App\Core\Application\Configuration;
+use App\Core\Framework\Enumerables\DataUnits;
+use App\Core\Server\FileManager;
+use App\Core\Server\Logger;
+
+/**
+ * The CacheManager class responsible for managing the cache.
+ */
+class CacheManager {
+
+	/**
+	 * Gets if the cache is valid for the specified key by checking the expiration time and cache file existence.
+	 * 
+	 * @param string $key The cache key.
+	 * @return bool True if the cache is valid, false otherwise.
+	 */
+	public static function isCacheValid($key): bool
+	{
+		$cacheFile = Configuration::CACHE_FOLDER . md5($key) . Configuration::CACHE_FILE_EXTENSION;
+		clearstatcache($cacheFile);
+		if (file_exists($cacheFile)) {
+			try {
+				$data = unserialize(file_get_contents($cacheFile));
+				if (isset($data['expires']) && is_int($data['expires'])) {
+					return $data['expires'] > time();
+				} else {
+					return false;
+				}
+			} catch (\Throwable $th) {
+				Logger::LogError("CacheManager", "Failed to read cache file: " . $th->getMessage());
+				return false;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Gets the cache data for the specified key.
+	 * 
+	 * @param string $key The cache key.
+	 * @return mixed|false The cache data if the cache is valid, false otherwise.
+	 */
+	public static function getCache($key): mixed|false {
+		if (self::isCacheValid($key)) {
+			$cacheFile = Configuration::CACHE_FOLDER . md5($key) . Configuration::CACHE_FILE_EXTENSION;
+			$data = unserialize(file_get_contents($cacheFile));
+			return $data['data'];
+		}
+		return false;
+	}
+
+	/**
+	 * Sets the cache data for the specified key.
+	 * 
+	 * @param string $key The cache key.
+	 * @param mixed $data The cache data.
+	 * @param int $expires The expiration time in seconds (default is 3600 seconds).
+	 * @return int|false The number of bytes written to the file, or false if failed (storage limit exceeded or max cache size exceeded).
+	 */
+	public static function writeCache($key, $data, $expires = 3600): int|false
+	{
+		if (!FileManager::isStorageUsageWithinLimits()) {
+			return false;
+		}
+		$cacheFile = Configuration::CACHE_FOLDER . md5($key) . Configuration::CACHE_FILE_EXTENSION;
+		$cacheData = ['data' => $data, 'expires' => time() + $expires];
+		$serializedData = serialize($cacheData);
+		FileManager::createFile($cacheFile, $serializedData, true);
+		clearstatcache($cacheFile);
+		if (FileManager::getFileSize($cacheFile, DataUnits::MEGABYTES) > Configuration::MAX_CACHE_SIZE_MB) {
+			unlink($cacheFile);
+			return false;
+		}
+		return FileManager::getFileSize($cacheFile, DataUnits::BYTES);
+	}
+
+	/**
+	 * Deletes the cache for the specified key.
+	 * 
+	 * @param string $key The cache key.
+	 * @return bool True if the cache was deleted, false otherwise.
+	 */
+	public static function deleteCache($key): bool
+	{
+		$cacheFile = Configuration::CACHE_FOLDER . md5($key) . Configuration::CACHE_FILE_EXTENSION;
+		if (file_exists($cacheFile)) {
+			return unlink($cacheFile);
+		}
+		return false;
+	}
+
+	/**
+	 * Deletes all cache files.
+	 * 
+	 * @return void
+	 */
+	public static function clearExpiredCache(): void
+	{
+		$files = glob(Configuration::CACHE_FOLDER . '*' . Configuration::CACHE_FILE_EXTENSION);
+		foreach ($files as $file) {
+			$data = unserialize(file_get_contents($file));
+			if ($data['expires'] < time()) {
+				unlink($file);
+			}
+		}
+	}
+
+	/**
+	 * Deletes all cache files.
+	 * 
+	 * @return void
+	 */
+	public static function clearAllCache(): void
+	{
+		$files = glob(Configuration::CACHE_FOLDER . '*' . Configuration::CACHE_FILE_EXTENSION);
+		foreach ($files as $file) {
+			unlink($file);
+		}
+	}
+}
