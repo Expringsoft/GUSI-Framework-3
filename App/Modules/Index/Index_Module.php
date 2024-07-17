9841d3572ac48fe75b0e1233e04961c6ee85dee9
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2024-07-17 02:49:01 -0600

    chore: Implementing favicon

--- App/Modules/Index/Index_Module.php
+++ App/Modules/Index/Index_Module.php
@@ -10,6 +10,7 @@ class Index_Module extends Module
 	static function registerRoutes()
 	{
 		Router::getInstance()->addRoute('/', Home::class);
+		Router::getInstance()->addRoute('/favicon.ico', [Home::class,'favicon']);
 	}
 
 	static function getFallback()
