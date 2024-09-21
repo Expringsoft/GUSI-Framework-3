9fe561ccdc810294ba99a3f13f79e69c41bcda9f
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2024-09-20 19:19:19 -0600

    chore(Regex): Added secure password regex and documentation for methods

--- App/Core/Framework/Classes/Regex.php
+++ App/Core/Framework/Classes/Regex.php
@@ -22,9 +22,17 @@ class Regex
 	public const REGEX_ONLY_ALPHA_NUMERIC_AND_NULL_WITH_SPACES = '/^[a-zA-Z0-9 ]*$/';
 	public const REGEX_ONLY_ALPHA_NUMERIC_WITH_SPACES = '/^[a-zA-Z0-9 ]+$/';
 	public const REGEX_ONLY_ALPHA_NUMERIC_WITH_SPACES_AND_NULL = '/^[a-zA-Z0-9 ]*$/';
-	
+	public const REGEX_SECURE_PASSWORD = '/^(?=.*[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/';
 	public const REGEX_VALID_BASE64_RFC4648 = '/^[-A-Za-z0-9+\/=]|=[^=]|={3,}$/';
 
+	/**
+	 * Returns a regex pattern for a varchar field.
+	 * 
+	 * @param int $minlenght The minimum lenght of the string.
+	 * @param int $maxlenght The maximum lenght of the string.
+	 * @param bool $allowNewLine If the string can contain new lines.
+	 * @return string The regex pattern.
+	 */
 	public static function varchar(int $minlenght, int $maxlenght, $allowNewLine = false)
 	{
 		if ($maxlenght == $minlenght) {
@@ -33,6 +41,13 @@ class Regex
 		return '/^' . ($allowNewLine ? '[\S\s]' : '[^\n]') . '{' . $minlenght . ',' . $maxlenght . '}$/';
 	}
 
+	/**
+	 * Returns a regex pattern limied to a specific capture group.
+	 * 
+	 * @param int $minlenght The minimum lenght of the string.
+	 * @param int $maxlenght The maximum lenght of the string.
+	 * @param array $groupElements The elements to capture (e.g. [a-z], [0-9], etc).
+	 */
 	public static function onlyCaptureGroup(int $minlenght, int $maxlenght, array $groupElements)
 	{
 		if ($maxlenght == $minlenght) {
@@ -41,6 +56,13 @@ class Regex
 		return '/^[' . implode($groupElements) . ']{' . $minlenght . ',' . $maxlenght . '}$/';
 	}
 
+	/**
+	 * Returns a regex pattern for an integer field.
+	 * 
+	 * @param int $minlenght The minimum lenght of the string.
+	 * @param int $maxlenght The maximum lenght of the string.
+	 * @return string The regex pattern.
+	 */
 	public static function int(int $minlenght, int $maxlenght)
 	{
 		if ($maxlenght == $minlenght) {
@@ -54,6 +76,13 @@ class Regex
 		return '/^[\S\s]*$/';
 	}
 
+	/**
+	 * Tests a string against a regex pattern.
+	 * 
+	 * @param string $pattern The regex pattern.
+	 * @param string $string The string to test.
+	 * @return int returns 1 if the pattern matches given subject, 0 if it does not, or FALSE if an error occurred.
+	 */
 	public static function test($pattern, $string)
 	{
 		return preg_match($pattern, $string);
